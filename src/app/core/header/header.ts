8b14a35c74d7f7e1d4e801c3dbbf7f6071363017
Dheeraj-Gawali <dheerajgawali98@gmail.com>
2026-04-04 12:04:47 +0530

    Fixed issue of responsive content

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -2,25 +2,26 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './header.html',
-  styleUrl: './header.scss'
+	selector: 'app-header',
+	imports: [RouterLink, RouterLinkActive],
+	templateUrl: './header.html',
+	styleUrl: './header.scss'
 })
 export class Header {
 
-  downloadbtn() {
-    const fileUrl = 'assets/Dheeraj_Gawali_Resume.pdf';
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-      if (isMobile) {
-        window.location.href = fileUrl;
-      } else {
-        const link = document.createElement('a');
-        link.href = fileUrl;
-        link.download = 'resume.pdf';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }
-  }
+	downloadResume(event: Event) {
+		const fileUrl = 'assets/Dheeraj_Gawali_Resume.pdf';
+		const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+		if (isMobile) {
+			// iOS fallback
+			window.open(fileUrl, '_blank');
+		} else {
+			const link = document.createElement('a');
+			link.href = fileUrl;
+			link.download = 'Dheeraj_Gawali_Resume.pdf';
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		}
+	}
 }
